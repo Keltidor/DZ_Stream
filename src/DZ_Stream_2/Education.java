c6b6f42c3fc1e16e5d6b1e53e627f84afebdf3bd
Vyacheslav Korostelev <koroslvyan@gmail.com>
2021-06-02 02:17:41 +0400

    Finished DZ

---
+++ src/DZ_Stream_2/Education.java
@@ -0,0 +1,8 @@
+package DZ_Stream_2;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ src/DZ_Stream_2/Sex.java
@@ -0,0 +1,6 @@
+package DZ_Stream_2;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
